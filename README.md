e95f136c4018075877df21bfb11bb3b5ae9b79b1
luizambrozini <64906691+luizambrozini@users.noreply.github.com>
2022-09-13 11:54:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-2r8pxn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-2r8pxn)
---
+++ index.ts
@@ -0,0 +1,58 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
+
+function initMap(): void {
+  const map = new google.maps.Map(
+    document.getElementById('map') as HTMLElement,
+    {
+      zoom: 12,
+      center: { lat: -23.26681, lng: -47.28159 },
+    }
+  );
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: '',
+    disableAutoPan: true,
+  });
+
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
+
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    marker.addListener('click', () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+
+    return marker;
+  });
+
+  // Add a marker clusterer to manage the markers.
+  new MarkerClusterer({ markers, map });
+}
+
+const locations = [
+  { lat: -23.26681, lng: -47.28159 },
+  { lat: -23.20526, lng: -47.2808 },
+];
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
+export {};
